331d10537566fc806c259b35bcd659c880cbb9d2
EmirAssy <al1609008@upy.edu.mx>
2019-06-06 14:07:22 -0500

    Almost done

--- controllers/robot1/robot1.c
+++ controllers/robot1/robot1.c
@@ -1,72 +1,59 @@
-/*
- * File:          robot1.c
- * Date:
- * Description:
- * Author:
- * Modifications:
- */
-
-/*
- * You may need to add include files like <webots/distance_sensor.h> or
- * <webots/differential_wheels.h>, etc.
- */
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/keyboard.h>
+#include <webots/distance_sensor.h>
 
-/*
- * You may want to add macros here.
- */
+#include <stdio.h>
+#include <math.h>
+
 #define TIME_STEP 64
 
-/*
- * This is the main program.
- * The arguments of the main function can be specified by the
- * "controllerArgs" field of the Robot node
- */
 int main(int argc, char **argv)
 {
-  /* necessary to initialize webots stuff */
+
   wb_robot_init();
+  wb_keyboard_enable(TIME_STEP);
 
-  /*
-   * You should declare here WbDeviceTag variables for storing
-   * robot devices like this:
-   *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
-   *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
-   */
+  int pressed_key;
 
+   // Motor devices
    WbDeviceTag wheel_right = wb_robot_get_device("motor_right");
    WbDeviceTag wheel_left = wb_robot_get_device("motor_left");
 
+   wb_motor_set_position (wheel_right, INFINITY);
+   wb_motor_set_position (wheel_left, INFINITY);
 
-
-   wb_motor_set_position(wheel_right,INFINITY);
-   wb_motor_set_position(wheel_left,INFINITY);
-  /* main loop
-   * Perform simulation steps of TIME_STEP milliseconds
-   * and leave the loop when the simulation is over
-   */
   while (wb_robot_step(TIME_STEP) != -1) {
 
-    /*
-     * Read the sensors :
-     * Enter here functions to read sensor data, like:
-     *  double val = wb_distance_sensor_get_value(my_sensor);
-     */
+     pressed_key= wb_keyboard_get_key();
 
-    /* Process sensor data here */
+     if (pressed_key == WB_KEYBOARD_UP)
+     {
+       wb_motor_set_velocity (wheel_right, -6.28);
+       wb_motor_set_velocity (wheel_left, -6.28);
+     }
+     else if (pressed_key == WB_KEYBOARD_DOWN)
+     {
+       wb_motor_set_velocity (wheel_right, 6.28);
+       wb_motor_set_velocity (wheel_left, 6.28);
+     }
+     else if (pressed_key == WB_KEYBOARD_RIGHT)
+     {
+       wb_motor_set_velocity(wheel_right, -6.28);
+       wb_motor_set_velocity(wheel_left, 6.28);
+     }
+     else if (pressed_key == WB_KEYBOARD_LEFT)
+     {
+       wb_motor_set_velocity(wheel_right, -6.28);
+       wb_motor_set_velocity(wheel_left, 6.28);
+     }
+     else
+     {
+       wb_motor_set_velocity (wheel_right, 0);
+       wb_motor_set_velocity (wheel_left, 0);
+     }
 
-    /*
-     * Enter here functions to send actuator commands, like:
-     * wb_differential_wheels_set_speed(100.0,100.0);
-     */
-     wb_motor_set_velocity(wheel_right,-5);
-     wb_motor_set_velocity(wheel_left,-5);
   };
-
-  /* Enter your cleanup code here */
-
-  /* This is necessary to cleanup webots resources */
   wb_robot_cleanup();
 
   return 0;
