41e60815fb4c0798293a582ff554ad826678bf23
EmirAssy <al1609008@upy.edu.mx>
2019-06-06 16:39:13 -0500

    Distance sensor finished

--- controllers/robot1/robot1.c
+++ controllers/robot1/robot1.c
@@ -3,8 +3,8 @@
 #include <webots/keyboard.h>
 #include <webots/distance_sensor.h>
 
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #define TIME_STEP 64
 
@@ -23,9 +23,17 @@ int main(int argc, char **argv)
    wb_motor_set_position (wheel_right, INFINITY);
    wb_motor_set_position (wheel_left, INFINITY);
 
+   //Distance sensor device
+   WbDeviceTag dist_sensor = wb_robot_get_device("distance_sensor");
+   wb_distance_sensor_enable(dist_sensor, TIME_STEP);
+   double ds_value;
+
   while (wb_robot_step(TIME_STEP) != -1) {
 
      pressed_key= wb_keyboard_get_key();
+
+     ds_value = wb_distance_sensor_get_value (dist_sensor);
+     printf("Distance sensor: %lf\n", ds_value);
 
      if (pressed_key == WB_KEYBOARD_UP)
      {
@@ -39,8 +47,8 @@ int main(int argc, char **argv)
      }
      else if (pressed_key == WB_KEYBOARD_RIGHT)
      {
-       wb_motor_set_velocity(wheel_right, -6.28);
-       wb_motor_set_velocity(wheel_left, 6.28);
+       wb_motor_set_velocity(wheel_right, 6.28);
+       wb_motor_set_velocity(wheel_left, -6.28);
      }
      else if (pressed_key == WB_KEYBOARD_LEFT)
      {
@@ -52,7 +60,8 @@ int main(int argc, char **argv)
        wb_motor_set_velocity (wheel_right, 0);
        wb_motor_set_velocity (wheel_left, 0);
      }
-
+     //wb_motor_set_velocity(wheel_right, 6.283);
+     //wb_motor_set_velocity(wheel_left, 6.283);
   };
   wb_robot_cleanup();
 
